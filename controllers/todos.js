7866a3d307a8b4fdde1bad3ddb3c383413550023
Michael Feeney <>
2022-09-02 02:27:00 +0000

    Many things were changed

--- controllers/todos.js
+++ controllers/todos.js
@@ -6,14 +6,16 @@ module.exports = {
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            // Gets all todos marked as complete
+            const public = await Todo.find({isPublic: true})
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user, public: public})
         }catch(err){
             console.log(err)
         }
     },
     createTodo: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id, isPublic: false})
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
@@ -38,6 +40,28 @@ module.exports = {
             })
             console.log('Marked Incomplete')
             res.json('Marked Incomplete')
+        }catch(err){
+            console.log(err)
+        }
+    },
+  markPublic: async (req, res)=>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                isPublic: true
+            })
+            console.log('Marked Public')
+            res.json('Marked Public')
+        }catch(err){
+            console.log(err)
+        }
+    },
+  markPrivate: async (req, res)=>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                isPublic: false
+            })
+            console.log('Marked Private')
+            res.json('Marked Private')
         }catch(err){
             console.log(err)
         }
--- models/Todo.js
+++ models/Todo.js
@@ -12,6 +12,10 @@ const TodoSchema = new mongoose.Schema({
   userId: {
     type: String,
     required: true
+  },
+  isPublic: {
+    type: Boolean,
+    required: true
   }
 })
 
